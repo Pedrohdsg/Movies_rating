ae3d4a6a1dfa04b5e315f67b3bfafba01460e4a6
Pedrohdsg <107958899+Pedrohdsg@users.noreply.github.com>
2023-03-29 17:14:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-//https://api.themoviedb.org/3/movie/550?api_key=62f6a077f19f6b94194adcb8d444f665
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "15c04215dfmsh47b152db5b5c176p1edfc4jsn455d1e039959",
+    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+  },
+};
+
+fetch("https://moviesdatabase.p.rapidapi.com/titles", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  //.then(response => console.log(response.results[0].titleText.text))
+  .catch((err) => console.error(err));
+
+const pesquisarFilme = document.getElementById("movie");
+
+console.log(pesquisarFilme);
+
+const pontuacaoFilme = document.getElementById('customRange2');
+
+console.log(pontuacaoFilme);
