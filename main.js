c131ca9f69359a7ad7ead33b28365880b4cf2c10
Pedrohdsg <107958899+Pedrohdsg@users.noreply.github.com>
2023-03-31 17:39:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "15c04215dfmsh47b152db5b5c176p1edfc4jsn455d1e039959",
-    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
-  },
-};
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "15c04215dfmsh47b152db5b5c176p1edfc4jsn455d1e039959",
+//     "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+//   },
+// };
 
-fetch("https://moviesdatabase.p.rapidapi.com/titles", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  //.then(response => console.log(response.results[0].titleText.text))
-  .catch((err) => console.error(err));
+// fetch("https://moviesdatabase.p.rapidapi.com/titles", options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   //.then(response => console.log(response.results[0].titleText.text))
+//   .catch((err) => console.error(err));
+
+const listaFilmes = [];
 
 const pesquisarFilme = document.getElementById("movieId");
 
-
 function avaliacao(pontuacaoFilme) {
   pontuacaoFilme = Number(document.getElementById("customRange2").value);
-  return pontuacaoFilme
+  return pontuacaoFilme;
 }
 
 filme = document.querySelector(".movie");
@@ -26,10 +27,21 @@ const inserir = document.getElementById("inserir");
 
 inserir.addEventListener("click", inserirFilme);
 
+const span = document.querySelector("#filmes");
+
 function inserirFilme() {
-  
+  listaFilmes.push({
+    nomeFilme: pesquisarFilme.value,
+    avaliaçãoFilme: avaliacao(),
+  });
   console.log(pesquisarFilme.value);
-  document.getElementById('filmes').innerHTML = `
-  <div>${pesquisarFilme.value} - ${avaliacao()} estrelas </div>
-  `
+  const li = document.createElement("li");
+  document.getElementById("lista-filmes").appendChild(li).innerHTML = `${pesquisarFilme.value} - ${avaliacao()} estrela(s)`;
+  
+  movieStorage = localStorage.setItem(listaFilmes, value)
+
+  
+
+  //console.log(li);
+  //console.log(listaFilmes);
 }
