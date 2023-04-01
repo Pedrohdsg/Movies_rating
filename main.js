733eacb4e799d9e01b1eaf1e963fc424f12dd9f5
Pedrohdsg <107958899+Pedrohdsg@users.noreply.github.com>
2023-04-01 17:28:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-// const options = {
-//   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Key": "15c04215dfmsh47b152db5b5c176p1edfc4jsn455d1e039959",
-//     "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
-//   },
-// };
-
-// fetch("https://moviesdatabase.p.rapidapi.com/titles", options)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   //.then(response => console.log(response.results[0].titleText.text))
-//   .catch((err) => console.error(err));
-
 const listaFilmes = [];
 
 const pesquisarFilme = document.getElementById("movieId");
@@ -29,19 +15,19 @@ inserir.addEventListener("click", inserirFilme);
 
 const span = document.querySelector("#filmes");
 
+
+
 function inserirFilme() {
   listaFilmes.push({
     nomeFilme: pesquisarFilme.value,
     avaliaçãoFilme: avaliacao(),
   });
   console.log(pesquisarFilme.value);
+
   const li = document.createElement("li");
-  document.getElementById("lista-filmes").appendChild(li).innerHTML = `${pesquisarFilme.value} - ${avaliacao()} estrela(s)`;
-  
- 
+  document.getElementById("lista-filmes").appendChild(li).innerHTML = `${
+    pesquisarFilme.value
+  } - ${avaliacao()} estrela(s)`;
 
-  
-
-  //console.log(li);
-  //console.log(listaFilmes);
+  localStorage.setItem("Lista de filmes", JSON.stringify(listaFilmes));
 }
